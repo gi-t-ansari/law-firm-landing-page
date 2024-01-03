86e244466acff558b8c8b1fce3e24d0249a6462f
gi-t-ansari <tanzeelshamsh.in@gmail.com>
2024-01-04 00:06:44 +0530

    added footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React from 'react';
+import styles from "./Footer.module.css";
+import Logo from '../Logo/Logo';
+import Navlist from '../Navlist/Navlist';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className={styles.footerWrapper}>
+      <section>
+        <div>
+          <Logo />
+        </div>
+        <Navlist />
+        <ul className={styles.socialListWrapper}>
+        <li>
+            <a href="https://www.linkedin.com/in/tanzeel-ansari-2a1327237/" rel='noreferrer' target='_blank'>
+              <LinkedInIcon />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/TanzeelShamsh" rel='noreferrer' target='_blank'>
+              <TwitterIcon />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/tanzeel_18.12?igshid=OGQ5ZDc2ODk2ZA==" rel='noreferrer' target="_blank">
+              <InstagramIcon />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/tanzeel-ansari-2a1327237/" rel='noreferrer' target="_blank">
+              <FacebookIcon />
+            </a>
+          </li>
+        </ul>
+      </section>
+      <div>
+        <span>© 2023 Tanzeel. All right reserved.</span>
+        <span>Privacy Policy</span>
+        <span>Terms of Service</span>
+      </div>
+    </footer>
   )
 }
 
--- src/components/SubscriptionSection/Subscription.jsx
+++ src/components/SubscriptionSection/Subscription.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./Subscription.module.css"
 
 function Subscription() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const getName = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+  }
+
+  const getEmail = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+  }
+
+  const submit = (e) => {
+    e.preventDefault();
+    if(name != "" && email != "") {
+      alert("Your Name & Email Captured");
+      setName("");
+      setEmail("");
+    } else {
+      alert("Enter name or email");
+    }
+    
+  }
+
+
+
+
   return (
     <section className={styles.subscriptionWrapper}>
         <h1>Subscribe Our Newsletter</h1>
-        <form className={styles.subscriptionForm}>
-            <input className={styles.subscriptionInput} style={{borderRadius: "7px 0px 0px 7px"}} type="text" placeholder='Name' />
-            <input className={styles.subscriptionInput} type="name" placeholder='Enter your Email' />
+        <form className={styles.subscriptionForm} onSubmit={submit}>
+            <input className={styles.subscriptionInput} style={{borderRadius: "7px 0px 0px 7px"}} type="text" placeholder='Name' onChange={getName} value={name} />
+            <input className={styles.subscriptionInput} type="name" placeholder='Enter your Email' onChange={getEmail} value={email} />
             <button type='submit' className={styles.subscriptionButton}>Send</button>
         </form>
     </section>
